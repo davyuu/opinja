e27ae6238816294c5d57fd85df839d6405c5c040
David Yu <davidyu_2005@hotmail.com>
2018-05-15 18:26:16 -0400

    rating

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -1,120 +1,21 @@
 import React from 'react'
 import gql from 'graphql-tag'
-import {graphql, compose} from 'react-apollo'
-import Modal from 'react-modal'
+import {graphql} from 'react-apollo'
 import MaterialIcon, {colorPallet} from 'material-icons-react'
-import ReactStars from 'react-stars'
+import RatingModal from '../components/RatingModal'
 import './Restaurant.css'
 
-Modal.setAppElement('#root')
-
 class Restaurant extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      modalIsOpen: false,
-      item: null,
-      rating: null,
-    }
-
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-    this.onRatingChange = this.onRatingChange.bind(this);
-    this.onRatingSubmit = this.onRatingSubmit.bind(this);
-  }
-
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
-
-  closeModal() {
-    this.setState({
-      modalIsOpen: false,
-      rating: null
-    });
-  }
-
-  onItemClick(item) {
-    this.setState({
-      item: item,
-      rating: item.rating
-    }, () => this.openModal())
-  }
-
-  onRatingChange(rating) {
-    this.setState({rating: rating})
-  }
-
-  onRatingSubmit() {
-    const {item, rating} = this.state;
-    if (rating === null) {
-      alert('Please select an option')
-      return
-    }
-
-    this.props.mutate({
-      variables: {
-        itemId: item.id,
-        rating: rating
-      },
-      update: (store, {data: {vote}}) => {
-        this.props.getRestaurant(this.props.data.restaurant.id)
-      }
-    }).then(({data}) => {
-      console.log('success', data)
-    }).catch(error => {
-      console.log('error', error)
-    })
-
-    this.closeModal()
-  }
 
   render() {
-    const {data} = this.props
-    if(data.loading) return <p>Loading...</p>
-
-    const {restaurant} = data
+    const {loading, restaurant, refetch} = this.props.data
+    if(loading) return <p>Loading...</p>
 
     if(!restaurant) {
       return (
         <p>No Restaurant Found</p>
       )
     }
-
-    const {item, rating} = this.state;
-    const itemName = item ? item.name : 'this item';
-    const modal = (
-      <Modal
-        isOpen={this.state.modalIsOpen}
-        onRequestClose={this.closeModal}
-        className='restaurant-modal'
-        overlayClassName='restaurant-modal-overlay'
-      >
-        <div className='restaurant-rating-container'>
-          <p className='restaurant-rating-title'>
-            How would you rate
-          </p>
-          <p className="restaurant-rating-title">
-            {itemName}?
-          </p>
-          <div className='restaurant-rating'>
-            <ReactStars
-              count={5}
-              value={rating}
-              onChange={this.onRatingChange}
-              size={60}
-            />
-          </div>
-          <div
-            className='restaurant-rating-submit'
-            onClick={this.onRatingSubmit}
-          >
-            Submit
-          </div>
-        </div>
-      </Modal>
-    )
 
     return (
       <div className='restaurant'>
@@ -139,7 +40,7 @@ class Restaurant extends React.Component {
               <div
                 key={i}
                 className='restaurant-list-item'
-                onClick={() => this.onItemClick(item)}
+                onClick={() => this.modal.openModal(item)}
               >
                 {/* <div className='restaurant-list-item-fill'/> */}
                 <p className='restaurant-list-item-name'>{item.name}</p>
@@ -148,13 +49,16 @@ class Restaurant extends React.Component {
             )
           })}
         </div>
-        {modal}
+        <RatingModal
+          onRef={ref => this.modal = ref}
+          refresh={() => refetch()}
+        />
       </div>
     )
   }
 }
 
-const query = gql`
+const QUERY_RESTAURANTS = gql`
   query getRestaurant($id: Int!) {
     restaurant(id: $id) {
       id
@@ -167,26 +71,13 @@ const query = gql`
     }
   }
 `
-const mutation = gql`
-  mutation addRating($itemId: Int!, $rating: Float!) {
-    addRating(itemId: $itemId, rating: $rating) {
-      id
-      itemId
-      rating
-    }
-  }
-`
 
-const queryOptions = {
+const OPTIONS_RESTAURANTS = {
   options: props => ({
     variables: {
       id: props.match.params.id
-    },
-    forceFetch: true
+    }
   })
 }
 
-export default compose(
-  graphql(query, queryOptions),
-  graphql(mutation)
-)(Restaurant)
+export default graphql(QUERY_RESTAURANTS, OPTIONS_RESTAURANTS)(Restaurant)
