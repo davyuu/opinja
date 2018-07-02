e920793f93d1abaf510c95cc74c95e87a2aab3ad
David Yu <davidyu_2005@hotmail.com>
2018-07-02 12:32:44 -0400

    order

--- src/constants/sum_categories.js
+++ src/constants/sum_categories.js
@@ -3,21 +3,21 @@ const categories = {
     name: 'Lunch Appetizers',
     image: 'appetizers'
   },
-  dinner_appetizers: {
-    name: 'Dinner Appetizers',
-    image: 'appetizers'
-  },
   lunch_mains: {
     name: 'Lunch Mains',
-    image: 'mains'
-  },
-  dinner_mains: {
-    name: 'Dinner Mains',
     image: 'mains'
   },
   lunch_desserts: {
     name: 'Lunch Desserts',
     image: 'desserts'
+  },
+  dinner_appetizers: {
+    name: 'Dinner Appetizers',
+    image: 'appetizers'
+  },
+  dinner_mains: {
+    name: 'Dinner Mains',
+    image: 'mains'
   },
   dinner_desserts: {
     name: 'Dinner Desserts',
